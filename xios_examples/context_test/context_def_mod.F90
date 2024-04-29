5b0eedd940932afc7c5114e8d8fba305b7583b1b
Harry Shepherd <harry.shepherd@metoffice.gov.uk>
2024-04-29 16:41:51 +0100

    Add review comments

--- xios_examples/context_test/context_def_mod.F90
+++ xios_examples/context_test/context_def_mod.F90
@@ -1,6 +1,9 @@
 module context_def_mod
-
-!use xios
+!
+! Read in the parameters for contexts that terminate before the end of the
+! model run from namelist, and load each into the context_struct derived
+! type, and make an array of each context.
+!
 
 use custom_type_mod, only : context_struct
 
@@ -24,6 +27,17 @@ subroutine load_contexts()
   integer :: i
 
   type(context_struct) :: my_context_struct
+
+  !
+  ! Read in the CUSTOM_CONTEXTS namelist from the file context.nml, and
+  ! construct a list of context_struct types for each context defined in
+  ! the namelist. The namelist contains 3 variables:
+  ! integer: n_contexts: the number of contexts to be read in
+  ! character(len=20): context_names(4): the name of each custom context
+  ! integer: closing_timesteps: the timestep on which the corresponding timestep
+  !                             in the context_names array is closed.
+  ! There are a maximum of 4 contexts, and only the first n_contexts are read in
+  !
 
   namelist /CUSTOM_CONTEXTS/ n_contexts, context_names, closing_timesteps
 
